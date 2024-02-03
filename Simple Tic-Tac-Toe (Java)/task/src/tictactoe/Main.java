cc9c019687f47c9cb854882f15b25a9ca7c979b4
Max Pestov <pestov.maxik@gmail.com>
2024-02-03 18:55:34 +0400

    The second stage completed

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try (var writer = new PrintWriter(System.out);
-             var reader = new Scanner(System.in)) {
+        try (var reader = new Scanner(System.in)) {
+            var writer = new PrintWriter(System.out);
             TicTacToeGame game = new TicTacToeGame(writer, reader);
 
             game.play();
--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/TicTacToeGame.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/TicTacToeGame.java
@@ -15,29 +15,39 @@ public class TicTacToeGame implements Game {
     public TicTacToeGame(PrintWriter writer, Scanner reader) {
         this.writer = writer;
         this.reader = reader;
-        initBattlefield();
+//        initBattlefield();
     }
 
     private void initBattlefield() {
-        Arrays.fill(battleFieldMatrix[0], 'X');
-        Arrays.fill(battleFieldMatrix[1], 'O');
-        Arrays.fill(battleFieldMatrix[2], 'X');
+        Arrays.fill(battleFieldMatrix[0], '_');
+        Arrays.fill(battleFieldMatrix[1], '_');
+        Arrays.fill(battleFieldMatrix[2], '_');
     }
 
     @Override
     public void play() {
+        String line = reader.nextLine();
+        for (int chIdx = 0, i = 0,j = 0; chIdx < BATTLEFIELD_SIZE * BATTLEFIELD_SIZE; chIdx++) {
+            this.battleFieldMatrix[i][j] = line.charAt(chIdx);
+            if (j++ == 2) {
+                i++;
+                j = 0;
+            }
+        }
         printTheField();
     }
 
     private void printTheField() {
+        writer.println("---------");
         for (int y = 0; y < BATTLEFIELD_SIZE; y++) {
+            writer.print("| ");
             for (int x = 0; x < BATTLEFIELD_SIZE; x++) {
                 writer.print(battleFieldMatrix[y][x]);
-                if (x < BATTLEFIELD_SIZE - 1) {
-                    writer.print(' ');
-                }
+                writer.print(' ');
             }
-            writer.println();
+            writer.println("|");
         }
+        writer.println("---------");
+        writer.flush();
     }
 }
