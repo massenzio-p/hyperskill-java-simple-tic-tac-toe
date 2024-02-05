1f9dc977c15953f6b03fa34f29f46723dc1c9ce3
Max Pestov <pestov.maxik@gmail.com>
2024-02-05 16:48:41 +0400

    The fourth stage completed

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/TicTacToeGame.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/TicTacToeGame.java
@@ -3,6 +3,8 @@ package tictactoe;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class TicTacToeGame implements Game {
 
@@ -25,10 +27,52 @@ public class TicTacToeGame implements Game {
         this.battleFieldMatrix = line.toCharArray();
 
         printTheField();
-        GameState state = this.gameAnalyzer.analyzeGameState(this.battleFieldMatrix);
-        String stateMsg = mapStateToMessage(state);
-        writer.println(stateMsg);
-        writer.flush();
+        acceptPlayerMove(reader, writer);
+        printTheField();
+
+//        GameState state = this.gameAnalyzer.analyzeGameState(this.battleFieldMatrix);
+//        String stateMsg = mapStateToMessage(state);
+//        writer.println(stateMsg);
+//        writer.flush();
+    }
+
+    private void acceptPlayerMove(Scanner reader, PrintWriter writer) {
+        StringTokenizer tokenizer;
+        int[] move;
+        while (true) {
+            try {
+                tokenizer = new StringTokenizer(reader.nextLine(), " ");
+                move = new int[] {
+                        Integer.parseInt(tokenizer.nextToken()),
+                        Integer.parseInt(tokenizer.nextToken())
+                };
+            } catch (NumberFormatException e) {
+                writer.println("You should enter numbers!");
+                writer.flush();
+                continue;
+            }
+            boolean outOfRange = false;
+            for (var coord : move) {
+                if (coord < 1 || coord > 3) {
+                    outOfRange = true;
+                    break;
+                }
+            }
+            if (outOfRange) {
+                writer.println("Coordinates should be from 1 to 3!");
+                writer.flush();
+                continue;
+            }
+            int actualIndex = (move[0] - 1) * BATTLEFIELD_SIZE + move[1] - 1;
+            if (this.battleFieldMatrix[actualIndex] != '_') {
+                writer.println("This cell is occupied! Choose another one!");
+                writer.flush();
+                continue;
+            } else {
+                this.battleFieldMatrix[actualIndex] = 'X';
+            }
+            return;
+        }
     }
 
     private String mapStateToMessage(GameState state) {
@@ -47,7 +91,11 @@ public class TicTacToeGame implements Game {
             if (x == 0) {
                 writer.print("| ");
             }
-            writer.print(this.battleFieldMatrix[i]);
+            if (this.battleFieldMatrix[i] == '_') {
+                writer.print(" ");
+            } else {
+                writer.print(this.battleFieldMatrix[i]);
+            }
             writer.print(' ');
             if (x++ == BATTLEFIELD_SIZE - 1) {
                 writer.println("|");
